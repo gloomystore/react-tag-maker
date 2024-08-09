dd28fcdac11da5f062b4a70475d3d929eb29b746
대영 이 <pikachu3333@naver.com>
2024-08-10 01:21:43 +0900

    feat: 1.0.18 - add type declare

---
+++ src/lib/index.d.ts
@@ -0,0 +1,7 @@
+import { GloomyTags } from './components/GloomyTags.d'
+// 이 모듈의 모든 export를 명시적으로 타입 정의합니다.
+
+// GloomyTags가 default export로 되어있기 때문에,
+// 여기서도 default로 export되도록 타입 정의합니다.
+
+export default GloomyTags;
